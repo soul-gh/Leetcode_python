d85f2f64fc3bb068b9e86bdf3650d18eaf944ccf
soul <g373733920@gmail.com>
2021-06-20 17:59:54 +0900

    167,169

---
+++ array/167.two_sum2.py
@@ -0,0 +1,28 @@
+#超时
+class Solution:
+    def twoSum(numbers, target):
+        res = [0,0]
+        for i in range(len(numbers)-1):
+            for j in range(i+1,len(numbers)):
+                if numbers[i] + numbers[j] == target:
+                    res[0] = i + 1
+                    res[1] = j + 1
+                    return res
+numbers = [2,3,5,6,7]
+target = 11
+print(Solution.twoSum(numbers,target))
+
+class Solution:
+    def twoSum(numbers, target):
+        N = len(numbers)
+        left,right = 0,N-1
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left+1,right+1]
+            elif numbers[left] + numbers[right] >target:
+                right -= 1
+            else:
+                left += 1
+numbers = [2,3,5,6,7]
+target = 11
+print(Solution.twoSum(numbers,target))
---
+++ array/169.majorityElement.py
@@ -0,0 +1,33 @@
+#超时
+class Solution:
+    def majorityElement(nums):
+        max_cnt = max_num = 0
+        for i in range(len(nums)):
+            cnt = 0
+            for j in range(len(nums)):
+                if nums[j] == nums[i]:
+                    cnt += 1
+            if cnt > max_cnt:
+                max_cnt = cnt
+                max_num = nums[i]
+        return max_num
+nums = [1,1,1,1,1,5,5,5,5,5,5,5,5,5]
+print(Solution.majorityElement(nums))
+
+#摩尔投票
+class Solution:
+    def majorityElement(nums):
+        N = len(nums)
+        cnt = 0
+        target_num = nums[0]
+        for num in nums:
+            if num == target_num:
+                cnt += 1
+            elif num != target_num:
+                cnt -= 1
+            if cnt == -1:
+                target_num = num
+                cnt = 0
+        return target_num
+nums = [1,1,1,1,1,5,5,5,5,5,5,5,5,5]
+print(Solution.majorityElement(nums))
