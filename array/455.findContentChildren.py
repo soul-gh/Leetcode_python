a1e9f79049e52fa07e16c8ff9d79f459aac0a669
soul <g373733920@gmail.com>
2021-06-29 14:01:44 +0900

    455

---
+++ array/455.findContentChildren.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findContentChildren(g, s):
+        cnt = 0
+        g.sort()
+        s.sort()
+        i = j = 0
+        while i < len(g) and j < len(s):
+            if g[i] <= s[j]:
+                cnt += 1
+                i += 1
+                j += 1
+            else:
+                j += 1
+        return cnt
+g = [1,2,3]
+s = [1,1]
+print(Solution.findContentChildren(g,s))
