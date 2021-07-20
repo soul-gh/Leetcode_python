1418221fbe9a0cd69e0fce3d05bbabdc3cf12dd8
soul <g373733920@gmail.com>
2021-07-20 22:58:24 +0900

    16

---
+++ offer/16.myPow.py
@@ -0,0 +1,33 @@
+#实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，x^n）。
+#超时
+class Solution:
+    def myPow(x: float, n: int) -> float:
+        if n == 0:
+            return float(1)
+        if n > 0:
+            res = x
+            for i in range(1,n):
+                res *= x
+            return res
+        if n < 0:
+            res = x
+            for i in range(1,-n):
+                res *= x
+            return 1/res
+print(Solution.myPow(2.0,0))
+
+#递归
+# n & 1 等于1 则n为奇数
+# 幂数分为奇和偶，奇数化为x*偶数次幂。 偶数则化为x^2*(n/2).（为了实现每次递归二分）
+class Solution:
+    def myPow(x: float, n: int) -> float:
+        if n == 0:
+            return float(1)
+        elif n < 0:
+            return 1 / Solution.myPow(x,-n)
+        elif n & 1:
+            return x * Solution.myPow(x,n-1)
+        else:
+            return Solution.myPow(x*x,n//2) 
+print(Solution.myPow(2.0,10))
+
