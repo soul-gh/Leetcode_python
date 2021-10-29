9d034546d76e43c1566869b3e146d2496db7821e
soul <g373733920@gmail.com>
2021-10-29 11:12:18 +0900

    206

---
+++ list/206.reverseList.py
@@ -0,0 +1,23 @@
+#给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(head: ListNode) -> ListNode:
+        cur,pre = head,None
+        while cur:
+            tmp = cur.next
+            cur.next = pre 
+            pre,cur = cur,tmp
+        return pre
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+head = Solution.reverseList(head)
+while head:
+    print(head.val)
+    head = head.next
