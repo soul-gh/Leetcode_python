a095ecac5635a759a75a80785d7e32a9868a0b3c
soul <g373733920@gmail.com>
2021-11-03 12:01:21 +0900

    02.07

---
+++ list/02.07.getIntersectionNode.py
@@ -0,0 +1,27 @@
+#给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+
+#双指针，一边结束到另一边开头，直到相遇
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(headA: ListNode, headB: ListNode) -> ListNode:
+        A,B = headA,headB
+        while A != B:
+            A = A.next if A else headB
+            B = B.next if B else headA
+        return A
+headA = ListNode(4)
+headA.next = ListNode(1)
+headA.next.next = ListNode(8)
+headA.next.next.next = ListNode(4)
+headA.next.next.next.next = ListNode(5)
+headB = ListNode(5)
+headB.next = ListNode(6)
+headB.next.next = ListNode(1)
+headB.next.next.next = ListNode(8)
+headB.next.next.next.next = ListNode(4)
+headB.next.next.next.next.next = ListNode(5)
+p = Solution.getIntersectionNode(headA,headB)
