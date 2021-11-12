90f8d537e4a1466b168a43d953ff9159deb448a1
soul <g373733920@gmail.com>
2021-11-12 11:24:44 +0900

    203

---
+++ list/203.removeElements.py
@@ -0,0 +1,19 @@
+#给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeElements(head: ListNode, val: int) -> ListNode:
+        if not head:
+            return head
+        while head and head.val == val:
+                head = head.next
+        cur = head
+        while cur and cur.next:
+            if cur.next.val == val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return head
